0a183dea7c8545ba14264794551ef6948c7f0085
Pooja Sharma <poonam.125.sharma@gmail.com>
2021-05-30 00:26:47 +0530

    Create oxytoken_oracle.sol

---
+++ oxytoken_oracle.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract OxyTokenSale_Oracle {
+    uint256 public _tokenPrice;
+    
+    function setTokenPrice(uint256 _price) external {
+        _tokenPrice = _price;
+    }
+    
+    function getTokenPrice() external view returns(uint256) {
+        return _tokenPrice;
+    }
+    
+}
